300187c727a1e2c2424e41ed9c6345d9d9d000fd
Ritchelle Grace Posadas <ritchelle.posadas@sap.com>
2019-11-23 16:26:04 -0500

    rename folder to foodie

--- src/main/java/foodie/Application.java
+++ src/main/java/foodie/Application.java
@@ -1,4 +1,4 @@
-package hello;
+package foodie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/foodie/Greeting.java
+++ src/main/java/foodie/Greeting.java
@@ -1,4 +1,4 @@
-package hello;
+package foodie;
 
 public class Greeting {
 
