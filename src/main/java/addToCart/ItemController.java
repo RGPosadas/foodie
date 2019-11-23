8b3cc8df868d3271e4c60f6d9e596b0d7f0712c7
Alessandro <kalessandro14@gmail.com>
2019-11-23 17:41:56 -0500

    cleaned up code

--- src/main/java/addToCart/ItemController.java
+++ src/main/java/addToCart/ItemController.java
@@ -17,18 +17,11 @@ public class ItemController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    // @RequestMapping("/items")
-    // public Greeting greeting(@RequestParam(value="restaurant_id") String name) {
-    //     return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    // }
-
     @RequestMapping(value = "/items", method = RequestMethod.GET)
     @ResponseBody
-    public List<Item> getRestaurentMenuItems() {
-        //get items by menu_id (a parameter of the URL)
-        int menu_id = 5; //@RequestParam(value="restaurant_id");
+    public List<Item> getRestaurentMenuItems(@RequestParam(name = "menu_id") int menu_id) {
         return jdbcTemplate.query(
-                    "SELECT * FROM Restaurant WHERE id = " + menu_id, 
+                    "SELECT * FROM Item WHERE menu_id = " + menu_id, 
                     (rs, rowNum) ->
                         new Item(
                             rs.getLong("id"),
