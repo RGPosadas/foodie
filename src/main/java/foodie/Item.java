4ac0d22ffdb35646bc2880754b1132239dfc60eb
Alessandro <kalessandro14@gmail.com>
2019-11-23 17:17:28 -0500

    started db connection handling

--- src/main/java/foodie/Item.java
+++ src/main/java/foodie/Item.java
@@ -2,20 +2,16 @@ package foodie;
 
 public class Item {
 
-    private final long id;
-    private final String name;
-    private final double price;
-    private final String description;
+    private  long id;
+    private  String name;
+    private  double price;
+    private  String description;
     //menu_id
 
-    public Item() {
-        this.id = 4;
-        this.name = "hi";
-        this.price = 0.1;
-        this.description = "test description";
-    }
-
-    public long getId() {
-        return id;
+    public Item(long id, String name, double price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
     }
 }
--- src/main/java/foodie/ItemController.java
+++ src/main/java/foodie/ItemController.java
@@ -1,25 +1,42 @@
 package foodie;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class ItemController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     // @RequestMapping("/items")
     // public Greeting greeting(@RequestParam(value="restaurant_id") String name) {
     //     return new Greeting(counter.incrementAndGet(), String.format(template, name));
     // }
 
-    // @RequestMapping(value = "/items", method = RequestMethod.GET)
-    // @ResponseBody
-    // public String getRestaurentMenuItems() {
-    //     //get items by menu_id (a parameter of the URL)
-    //     Item[] items = jdbcTemplate.queryForObject("SELECT * FROM Restaurant WHERE id == " + @RequestParam(value="restaurant_id"), Integer.class);
-    //     return items;
-    // }
+    @RequestMapping(value = "/items", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Item> getRestaurentMenuItems() {
+        //get items by menu_id (a parameter of the URL)
+        int menu_id = 5; //@RequestParam(value="restaurant_id");
+        return jdbcTemplate.query(
+                    "SELECT * FROM Restaurant WHERE id = " + menu_id, 
+                    (rs, rowNum) ->
+                        new Item(
+                            rs.getLong("id"),
+                            rs.getString("name"),
+                            rs.getDouble("price"),
+                            rs.getString("description")
+                        )
+            );
+    }
 }
+
