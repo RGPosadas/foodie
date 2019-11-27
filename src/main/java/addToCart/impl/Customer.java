2b2eab40b022673d5cf73e29ac67f4141d735202
Ritchelle Grace Posadas <ritchelle.posadas@sap.com>
2019-11-27 12:02:53 -0500

    change everything ot int

--- src/main/java/addToCart/impl/Customer.java
+++ src/main/java/addToCart/impl/Customer.java
@@ -2,14 +2,14 @@ package addToCart;
 
 public class Customer {
 
-    private long id;
+    private int id;
     private String name;
     private String location;
     private String username;
     private String password;
     private String email;
 
-    public Customer(long id, String name, String location, String username, String password, String email) {
+    public Customer(int id, String name, String location, String username, String password, String email) {
         this.id = id;
         this.name = name;
         this.location = location;
--- src/main/java/addToCart/impl/Item.java
+++ src/main/java/addToCart/impl/Item.java
@@ -2,12 +2,12 @@ package addToCart.impl;
 
 public class Item {
 
-    private  long id;
+    private  int id;
     private  String name;
     private  double price;
     private  String description;
 
-    public Item(long id, String name, double price, String description) {
+    public Item(int id, String name, double price, String description) {
         this.id = id;
         this.name = name;
         this.price = price;
