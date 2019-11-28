51ca8b5f0d6300e427da15a7a44cbb8014736dd3
davidchen2171 <davidchen_98@hotmail.com>
2019-11-28 15:11:07 -0500

    adding necessary components

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -2,8 +2,18 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
+import {AppRegistry, Text, View, Navigator} from 'react-native';
+import ProvideAddress from './components/ProvideAddress';
+import FetchRestaurants from './components/FetchRestaurants';
+import FetchMenu from './components/FetchMenu';
+import GoToCart from './components/GoToCart';
+
 import {name as appName} from './app.json';
+
+export default class appName extends Component{
+    renderScene(route, navigator){
+        switch(route.id)
+    }
+}
 
 AppRegistry.registerComponent(appName, () => App);
