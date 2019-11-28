599b3007ac3c053d0541dfe86636a72e276814b6
davidchen2171 <davidchen_98@hotmail.com>
2019-11-28 16:22:10 -0500

    fix components

---
+++ AwesomeProject/components/FetchMenu.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {AppRegistry, Text, View, Button} from 'react-native';
+
+export default class FetchMenu extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name: this.props.user.name,
+            email: this.props.user.email
+        }
+    }
+
+    onPress(){
+        this.props.navigator.push({
+            id:'fetchMenu'
+        });
+    }
+
+    render(){
+        return(
+        <View>
+            <Text>{this.state.name}</Text>
+            <Text>{this.state.email}</Text>
+            <Button
+                onPress={this.onPress.bind(this)}
+                title="Go Back"
+            />
+        </View>
+        );
+    }
+}
--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -3,17 +3,33 @@
  */
 
 import {AppRegistry, Text, View, Navigator} from 'react-native';
+import React, {Component} from 'react';
+
 import ProvideAddress from './components/ProvideAddress';
 import FetchRestaurants from './components/FetchRestaurants';
 import FetchMenu from './components/FetchMenu';
 import GoToCart from './components/GoToCart';
 
-import {name as appName} from './app.json';
 
-export default class appName extends Component{
+export default class AwesomeProject extends Component{
     renderScene(route, navigator){
-        switch(route.id)
+        switch(route.id){
+            case 'fetchRestaurants':
+                return (<FetchRestaurants navigator={navigator} title="fetchRestaurants" />)
+            case 'fetchMenu':
+                return (<FetchMenu user={route.user} navigator={navigator} title="fetchMenu" />)
+        }
+    }
+
+    render(){
+        return(
+            <Navigator
+                initialRoute={{id: 'restaurants'}}
+                renderScene={this.renderScene}
+                configureScreen={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
+            />
+        );
     }
 }
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
