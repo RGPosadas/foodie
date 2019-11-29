782f0bde29c5cb12661af7e84ee2022fc3e578b3
davidchen2171 <davidchen_98@hotmail.com>
2019-11-29 01:32:14 -0500

    added display items

--- AwesomeProject/components/FetchMenu.js
+++ AwesomeProject/components/FetchMenu.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import {ActivityIndicator, Button, Text, View} from "react-native";
+import {ActivityIndicator,
+        Button,
+        Text,
+        View,
+        FlatList,
+        StyleSheet,
+        SafeAreaView,
+        TouchableHighlight}
+        from "react-native";
 
 export default class FetchMenu extends Component {
 
@@ -22,6 +30,12 @@ export default class FetchMenu extends Component {
             },
         ];
     }
+//    const data = {foo:1, bar:2};
+//
+//    fetch('https://api.parse.com/1/users?foo=${encodeURIComponent(data.foo)}&bar=${encodeURIComponent(data.bar)}', {
+//        method: "GET",
+//        headers: headers,
+//    })
 
     render(){
 
@@ -31,6 +45,24 @@ export default class FetchMenu extends Component {
         return (
             <View style={{ flex: 0, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>Fetch Menu {JSON.stringify(resto)}</Text>
+
+                <View style={styles.container}>
+                      <FlatList
+//                        data={this.state.dataSource}  add this after having real data
+                          data={this.cartItems}
+                          renderItem={({item}) => {
+                               return(
+                                    <TouchableHighlight onPress={() => alert("Added") }
+                                    underlayColor='white'>
+                                        <Text style={styles.item}>{item.name} {item.price}</Text>
+                                     </TouchableHighlight>
+
+                                    )
+                               }
+                          }
+                      />
+                </View>
+
                 <Button
                     title="GoToCart"
                     onPress={() => navigate('GoToCart')}
@@ -39,3 +71,16 @@ export default class FetchMenu extends Component {
         );
     }
 };
+
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
